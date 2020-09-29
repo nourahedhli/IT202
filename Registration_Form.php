7c0850ce2a4d2e9b2fc9e9ca49b7078f1bc80535
Noura Hedhli <nh98@afsconnect1.njit.edu>
2020-09-29 10:55:50 -0400

    Registration_Form with db

--- Registration_Form.php
+++ Registration_Form.php
@@ -47,4 +47,29 @@ if(isset($_POST["register"])){
    echo "There was a validation issue"; 
   }
 }
+if($isValid){
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+    require_once("db.php");
+    $db = getDB();
+	if(isset($db)){
+		//here we'll use placeholders to let PDO map and sanitize our data
+		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+		//here's the data map for the parameter to data
+		$params = array(":email"=>$email, ":password"=>$hash);
+		$r = $stmt->execute($params);
+		//let's just see what's returned
+		echo "db returned: " . var_export($r, true);
+		$e = $stmt->errorInfo();
+		if($e[0] == "00000"){
+			echo "<br>Welcome! You successfully registered, please login.";
+		}
+		else{
+			echo "uh oh something went wrong: " . var_export($e, true);
+		}
+	}
+  }
+  else{
+   echo "There was a validation issue"; 
+  }
+}
 ?>
