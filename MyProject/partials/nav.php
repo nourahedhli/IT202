e90ed1d7149ec68f515cb392a6bbaae290f20470
Noura Hedhli <nh98@afsconnect1.njit.edu>
2020-11-13 20:00:46 -0500

    test files

--- MyProject/partials/nav.php
+++ MyProject/partials/nav.php
@@ -1,3 +1,4 @@
+
 <link rel="stylesheet" href="css/styles.css">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
@@ -10,8 +11,15 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+<?php if (has_role("Admin")): ?>
+            <li><a href="<?php echo getURL("test/test_create_product.php"); ?>">Create Product</a></li>
+            
+            
+            
+        <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
+        <li><a href="#"> Shop</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
--- MyProject/test/test_create_product.php
+++ MyProject/test/test_create_product.php
@@ -1,110 +1,52 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-
 <?php
-
 if (!has_role("Admin")) {
-
     //this will redirect to login and kill the rest of this script (prevent it from executing)
-
     flash("You don't have permission to access this page");
-
     die(header("Location: login.php"));
-
 }
-
 ?>
-
-
-
 
 <form method="POST">
-
 	<label>Name</label>
-
 	<input name="name" placeholder="Name"/>
-
 	<label>Quantity</label>
-      <input type="number" name="quantity "/>
-
-
-	<label>Price </label>
-
-	<input type="number" name="price "/>
-
-	<label>Description </label>
-
-	<input type="number" name="description"/>
-
-	<label>Modified</label>
-
-	<input type="number" name="modified"/>
-
+	<input type="int" min="1" name="quantity"/>
+	<label>Price</label>
+	<input type="decimal" min="1" name="price"/>
+	<label>Description</label>
+	<input type="text" min="1" name="description"/>
 	<input type="submit" name="save" value="Create"/>
-
 </form>
 
-
-
-
 <?php
-
 if(isset($_POST["save"])){
-
 	//TODO add proper validation/checks
-
 	$name = $_POST["name"];
-
 	$quantity = $_POST["quantity"];
-
-	$price = $_POST["price "];
-
-	$description = $_POST["description"];
-
-	$modified = $_POST["modified"];
-
-	$created = date('Y-m-d H:i:s');//calc
-
+	$br = $_POST["price"];
+	$min = $_POST["description"];
+	$max = date('Y-m-d H:i:s');
+	$nst = date('Y-m-d H:i:s');//calc
 	$user = get_user_id();
-
 	$db = getDB();
-
-	$stmt = $db->prepare("INSERT INTO Products  (name, quantity, price, description, modified, created, user_id) VALUES(:name, :quantity, :price, :description,:modified,:created,:user)");
-
+	$stmt = $db->prepare("INSERT INTO Products (name, quantity, price, description, modified, created, user_id) VALUES(:name, :quantity, :br, :min, :max, :nst,:user)");
 	$r = $stmt->execute([
-
 		":name"=>$name,
-
 		":quantity"=>$quantity,
-
-		":price"=>$price,
-
-		":description"=>$description,
-
-		":modified"=>$modified,
-
-		":created"=>$created,
-
+		":br"=>$br,
+		":min"=>$min,
+		"max"=>$max,
+		":nst"=>$nst,
 		":user"=>$user
-
 	]);
-
 	if($r){
-
 		flash("Created successfully with id: " . $db->lastInsertId());
-
 	}
-
 	else{
-
 		$e = $stmt->errorInfo();
-
 		flash("Error creating: " . var_export($e, true));
-
 	}
-
 }
-
 ?>
-
 <?php require(__DIR__ . "/partials/flash.php");
-
