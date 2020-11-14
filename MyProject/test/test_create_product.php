fea77f481d6f84d147531eb893a7ad7915fb5bc7
Noura Hedhli <nh98@afsconnect1.njit.edu>
2020-11-13 22:03:42 -0500

    test create

--- MyProject/test/test_create_product.php
+++ MyProject/test/test_create_product.php
@@ -1,52 +1,107 @@
 <?php require_once(__DIR__ . "/../partials/nav.php"); ?>
+
 <?php
+
 if (!has_role("Admin")) {
+
     //this will redirect to login and kill the rest of this script (prevent it from executing)
+
     flash("You don't have permission to access this page");
+
     die(header("Location: login.php"));
+
 }
+
 ?>
+
+
+
 
 <form method="POST">
+
 	<label>Name</label>
+
 	<input name="name" placeholder="Name"/>
+
 	<label>Quantity</label>
+
 	<input type="int" min="1" name="quantity"/>
+
 	<label>Price</label>
+
 	<input type="decimal" min="1" name="price"/>
+
 	<label>Description</label>
+
 	<input type="text" min="1" name="description"/>
+
 	<input type="submit" name="save" value="Create"/>
+
 </form>
 
+
+
+
 <?php
+
 if(isset($_POST["save"])){
+
 	//TODO add proper validation/checks
+
 	$name = $_POST["name"];
+
 	$quantity = $_POST["quantity"];
-	$br = $_POST["price"];
-	$min = $_POST["description"];
-	$max = date('Y-m-d H:i:s');
-	$nst = date('Y-m-d H:i:s');//calc
+
+	$price = $_POST["price"];
+
+	$description = $_POST["description"];
+
+	$modified = date('Y-m-d H:i:s');
+
+	$created = date('Y-m-d H:i:s');//calc
+
 	$user = get_user_id();
+
 	$db = getDB();
-	$stmt = $db->prepare("INSERT INTO Products (name, quantity, price, description, modified, created, user_id) VALUES(:name, :quantity, :br, :min, :max, :nst,:user)");
+
+	$stmt = $db->prepare("INSERT INTO Products (name, quantity, price, description, modified, created, user_id) VALUES(:name, :quantity, :price, :description, :modified, :created,:user)");
+
 	$r = $stmt->execute([
+
 		":name"=>$name,
+
 		":quantity"=>$quantity,
-		":br"=>$br,
-		":min"=>$min,
-		"max"=>$max,
-		":nst"=>$nst,
+
+		":price"=>$price,
+
+		":description"=>$description,
+
+		"modified"=>$modified,
+
+		":created"=>$created,
+
 		":user"=>$user
+
 	]);
+
 	if($r){
+
 		flash("Created successfully with id: " . $db->lastInsertId());
+
 	}
+
 	else{
+
 		$e = $stmt->errorInfo();
+
 		flash("Error creating: " . var_export($e, true));
+
 	}
+
 }
+
 ?>
+
 <?php require(__DIR__ . "/../partials/flash.php");
+
+
