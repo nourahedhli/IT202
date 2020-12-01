50289a82d0f80ff48755287a8bb6f5f4c2cd84ae
Noura Hedhli <nh98@afsconnect1.njit.edu>
2020-12-01 10:56:56 -0500

    cart for the user

--- MyProject/partials/nav.php
+++ MyProject/partials/nav.php
@@ -1,3 +1,4 @@
+
 <?php
 
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
@@ -6,50 +7,107 @@ require_once(__DIR__ . "/../lib/helpers.php");
 
 ?>
 
-<link rel="stylesheet" href="<?php echo getURL("css/styles.css"); ?>">
+<!-- CSS -->
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 
 
 
-<nav>
+<!-- jQuery and JS bundle w/ Popper.js -->
 
-    <ul class="nav">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
 
-        <li><a href="<?php echo getURL("home.php");?>">Home</a></li>
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
 
-        <?php if (!is_logged_in()): ?>
+        crossorigin="anonymous"></script>
 
-            <li><a href="<?php echo getURL("login.php"); ?>">Login</a></li>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
 
-            <li><a href="<?php echo getURL("register.php"); ?>">Register</a></li>
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
 
-        <?php endif; ?>
+        crossorigin="anonymous"></script>
 
-        <?php if (has_role("Admin")): ?>
 
-            <li><a href="<?php echo getURL("test/test_create_product.php"); ?>">Create Product</a></li>
 
-            <li><a href="<?php echo getURL("test/test_list_product.php"); ?>">View Products</a></li>            
-<li><a href="<?php echo getURL("test/test_create_cart.php"); ?>"> Cart</a></li>
 
-            
+<div class="container-fluid">
 
-            
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-        <?php endif; ?>
+        <ul class="navbar-nav mr-auto">
 
-        <?php if (is_logged_in()): ?>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("home.php"); ?>">Home</a></li>
 
-		
+            <?php if (!is_logged_in()): ?>
 
-            <li><a href="<?php echo getURL("profile.php"); ?>">Profile</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo getURL("login.php"); ?>">Login</a></li>
 
-            <li><a href="<?php echo getURL("logout.php"); ?>">Logout</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo getURL("register.php"); ?>">Register</a></li>
 
-        <?php endif; ?>
+            <?php endif; ?>
 
-    </ul>
+            <?php if (has_role("Admin")): ?>
 
-    
+                <li class="nav-item dropdown">
 
-</nav>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+
+                       data-toggle="dropdown"
+
+                       aria-haspopup="true" aria-expanded="false">
+
+                        Admin
+
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                        <a class="nav-link" href="<?php echo getURL("test/test_create_product.php"); ?>">Create
+
+                            Products</a>
+
+                        <a class="nav-link" href="<?php echo getURL("test/test_list_product.php"); ?>">View
+
+                            Products</a>
+
+                        <a class="nav-link" href="<?php echo getURL("test/test_create_cart.php"); ?>">Create
+
+                            Cart</a>
+
+
+
+
+                        <a class="nav-link" href="<?php echo getURL("test/test_list_cart.php"); ?>">View
+
+                            Cart</a>
+
+                    </div>
+
+                </li>
+
+            <?php endif; ?>
+
+            <?php if (is_logged_in()): ?>
+
+                <li class="nav-item"><a class="nav-link" href="<?php echo getURL("shop.php"); ?>">Shop</a></li>
+
+                <li class="nav-item"><a class="nav-link" href="<?php echo getURL("my_cart.php"); ?>">Cart</a></li>
+
+                <li class="nav-item"><a class="nav-link" href="<?php echo getURL("profile.php"); ?>">Profile</a></li>
+
+                <li class="nav-item"><a class="nav-link" href="<?php echo getURL("logout.php"); ?>">Logout</a></li>
+
+            <?php endif; ?>
+
+        </ul>
+
+        <span class="navbar-text">Balance: <?php echo getBalance(); ?></span>
+
+    </nav>
+
+</div>
+
+
