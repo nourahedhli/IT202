664f1589a8c42500e43f24c43b0e6d3b56b54434
Noura Hedhli <nh98@afsconnect1.njit.edu>
2020-12-14 23:37:58 -0500

    checkout

--- MyProject/checkout.php
+++ MyProject/checkout.php
@@ -1,6 +1,5 @@
 <?php require_once(__DIR__ . "/partials/nav.php");
-//this is the checkout page, accessed through a link in the cart
-//Will have form for payment method, shipping, and show the products being bought
+
 
 //only let's users access if logged in
 if (!is_logged_in()) {
