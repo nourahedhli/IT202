309fc466c0aed29e169e90abc5e801fb69d07edb
Noura Hedhli <nh98@afsconnect1.njit.edu>
2020-12-15 12:44:22 -0500

    fix

--- MyProject/sql/create_table_order.sql
+++ MyProject/sql/create_table_order.sql
@@ -3,7 +3,7 @@ CREATE TABLE Orders (
     user_id int,
     total_price decimal(12,2) default 0.00,
     created TIMESTAMP default current_timestamp,
-    address VARCHAR(100),
+    address VARCHAR(50),
     payment_method VARCHAR(60),
     primary key (id),
     foreign key (user_id) references Users (id)
